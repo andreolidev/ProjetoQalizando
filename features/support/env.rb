8c18ba52940001999825a1fd91a73895802bc9d0
andreolivdev <andreolive.91@gmail.com>
2024-08-23 22:01:54 -0300

    Automatização de testes

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,14 @@
-require 'cucumber'
+require 'capybara'
 require 'capybara/cucumber'
 require 'capybara/rspec'
 require 'site_prism'
+require 'site_prism/all_there'
 require 'rspec'
 require 'rspec/expectations'
+require 'rspec/core'
+
+Capybara.configure do |config|
+    config.default_driver = :selenium_chrome
+    config.app_host = 'http://www.linkedin.com/'
+    config.default_max_wait_time = 5
+end
